2821d1fdbd9092e1977f2e43db7824f1796a0637
m1tsuyaaa <stepan.katkovxx@gmail.com>
2026-02-05 19:39:52 +0700

    Убрал магическое число и выровнял отступы

--- Program.cs
+++ Program.cs
@@ -1,55 +1,52 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
-    {
-        int baseNumber, exponent, powerResult, secondCharIndex, startCounter, initialValue, originalNumber, transformedNumber;
-        string numberString, resultString;
+class Program {
+  static void Main() {
+    int baseNumber, exponent, powerResult, secondCharIndex, startCounter, initialValue, originalNumber, transformedNumber;
+    string numberString, resultString;
 
-        secondCharIndex = 1;
-        startCounter = 0;
-        initialValue = 1;
+    int minNumberForTransformation = 100;
 
-        Console.WriteLine("=== Task 1: Power calculation ===");
+    secondCharIndex = 1;
+    startCounter = 0;
+    initialValue = 1;
 
-        Console.Write("Enter base number: ");
-        baseNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Task 1: Power calculation");
 
-        Console.Write("Enter exponent: ");
-        exponent = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter base number: ");
+    baseNumber = Convert.ToInt32(Console.ReadLine());
 
-        if (baseNumber <= 0 || exponent <= 0)
-        {
-            Console.WriteLine("\nError: Both numbers must be natural (greater than zero)");
-        } else {
-            powerResult = initialValue;
+    Console.Write("Enter exponent: ");
+    exponent = Convert.ToInt32(Console.ReadLine());
 
-            for (int counter = startCounter; counter < exponent; ++counter)
-            {
-                powerResult = powerResult * baseNumber;
-            }
+    if (baseNumber <= 0 || exponent <= 0) {
+        Console.WriteLine("\nError: Both numbers must be natural (greater than zero)");
+    } else {
+      powerResult = initialValue;
 
-            Console.WriteLine($"\nResult: {baseNumber}^{exponent} = {powerResult}");
-        }
+      for (int counter = startCounter; counter < exponent; ++counter) {
+          powerResult = powerResult * baseNumber;
+      }
 
-        Console.WriteLine("\n=== Task 2: Number transformation ===");
+      Console.WriteLine($"\nResult: {baseNumber}^{exponent} = {powerResult}");
+      }
 
-        Console.Write("Enter number (must be >= 100): ");
-        originalNumber = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("\n=== Task 2: Number transformation ===");
 
-        if (originalNumber < 100)
-        {
-            Console.WriteLine("\nError: Input number must be >= 100 and contain more than two digits");
-        } else {
-            numberString = originalNumber.ToString();
-            resultString = numberString.Remove(secondCharIndex, 1) + numberString[secondCharIndex];
-            transformedNumber = int.Parse(resultString);
+      Console.Write("Enter number (must be >= 100): ");
+      originalNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"\nOriginal number: {originalNumber}");
-            Console.WriteLine($"Transformed number: {transformedNumber}");
-        }
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
-    }
+      if (originalNumber < minNumberForTransformation) {
+          Console.WriteLine("\nError: Input number must be >= 100 and contain more than two digits");
+      } else {
+        numberString = originalNumber.ToString();
+        resultString = numberString.Remove(secondCharIndex, 1) + numberString[secondCharIndex];
+        transformedNumber = int.Parse(resultString);
+
+        Console.WriteLine($"\nOriginal number: {originalNumber}");
+        Console.WriteLine($"Transformed number: {transformedNumber}");
+      }
+      Console.WriteLine("\nPress any key to exit...");
+      Console.ReadKey();
+  }
 }
